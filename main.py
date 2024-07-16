f0cf2a1a0295c1b1d630e35d57cbd4155ae1bdf0
Bradley Thompson <bradleyowenthompson@outlook.com>
2024-07-16 09:02:25 +0100

    refactor: optimisation changes

--- main.py
+++ main.py
@@ -46,36 +46,29 @@ words = [
 masks = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
 
-def is_viable(x_candidate, y_candidate, x_step, y_step):
+def is_viable(x_candidate: int, y_candidate: int, x_step: int, y_step: int) -> bool:
     """Check if a requested step is within the board"""
     max_y, max_x = game_grid.shape
 
-    if (
-        (x_candidate + (x_step) >= 0)
-        and (x_candidate + (x_step) < max_x)
-        and (y_candidate + (y_step) >= 0)
-        and (y_candidate + (y_step) < max_y)
-    ):
+    if (0 <= (x_candidate + x_step) < max_x) and (0 <= (y_candidate + y_step) < max_y):
         return True
     else:
         return False
 
 
-def follow_path(word, x_position, y_position, x_offset, y_offset):
+def follow_path(
+    word: str, x_position: int, y_position: int, x_offset: int, y_offset: int
+) -> bool:
     "Check if the journey leads to the word"
-    characters_matched = 0
-    for step, character in enumerate(word):
+    # Skip the first character, check if characters are in sequence
+    for step, character in enumerate(word[1:], start=1):
         if (
             character
-            == game_grid[y_position + (y_offset * step), x_position + (x_offset * step)]
+            != game_grid[y_position + (y_offset * step), x_position + (x_offset * step)]
         ):
-            characters_matched += 1
-        else:
-            break
-    if characters_matched == len(word):
-        return True
-    else:
-        return False
+            return False
+
+    return True
 
 
 # Find each word
@@ -94,10 +87,11 @@ for word in words:
                 ((len(word) - 1) * mask[0]),
                 ((len(word) - 1) * mask[1]),
             ):
-                # Check if it can follow a valid path
+                # Check if it follows a valid path
                 if follow_path(word, coordinate_x, coordinate_y, mask[0], mask[1]):
                     print(
                         f"{word}: ({coordinate_x}, {coordinate_y}) -> "
                         f"({coordinate_x+((len(word)-1) * mask[0])}, "
                         f"{coordinate_y + ((len(word)-1) * mask[1])})"
                     )
+                    break
